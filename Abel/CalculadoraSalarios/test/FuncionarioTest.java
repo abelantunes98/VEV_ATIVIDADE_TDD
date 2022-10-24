9067db7a5bd13d2ed830b69150c683fce41bc98e
Abel Antunes <abel.antunes@ibm.com>
2022-10-24 15:51:52 -0300

    Criando testes de Funcionario

--- Abel/CalculadoraSalarios/test/FuncionarioTest.java
+++ Abel/CalculadoraSalarios/test/FuncionarioTest.java
@@ -1,5 +1,56 @@
 package test;
 
-public class FuncionarioTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FuncionarioTest {
+	
+	
+	/* Teste de Gets e Sets */
+	
+	Funcionario funcionario = new Funcionario("Abel", "abel@email.com", 500, "DESENVOLVEDOR");
+	@Test
+    public void getNameTest(){
+        assertEquals("Abel", funcionario.getName());
+    }
+	
+	@Test
+    public void getEmailTest(){
+        assertEquals("abel@email.com", funcionario.getEmail());
+    }
+	
+	@Test
+    public void getSalaryTest(){
+        assertEquals(500, funcionario.getSalary());
+    }
+	
+	@Test
+    public void getJobTest(){
+        assertEquals("DESENVOLVEDOR", funcionario.getJob());
+    }
+	
+	@Test
+    public void setNameTest(){
+		funcionario.setName("Joao");
+        assertEquals("Joao", funcionario.getName());
+    }
+	
+	@Test
+    public void setEmailTest(){
+		funcionario.setEmail("joao@email.com");
+        assertEquals("joao@email.com", funcionario.getEmail());
+    }
+	
+	@Test
+    public void setSalaryTest(){
+		funcionario.setSalary(1000);
+        assertEquals(1000, funcionario.getSalary());
+    }
+	
+	@Test
+    public void setJobTest(){
+		funcionario.setName("DBA");
+        assertEquals("DBA", funcionario.getJob());
+    }
 }
